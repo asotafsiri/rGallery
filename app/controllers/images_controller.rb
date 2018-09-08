16ed1065f4af811c0dc5de5b84629f789bb9a708
asotafsiri <aso.tafsiri@gmail.com>
2018-09-08 22:37:56 +0430

    handeling image loading and inserting to database

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -16,7 +16,7 @@ class ImagesController < ApplicationController
     @image = Image.new(image_params)
     if @image.save
       flash[:notice] = 'Image Created'
-      redirect_to images_path
+      redirect_to root_path
     else
       render 'new'
     end
@@ -27,9 +27,10 @@ class ImagesController < ApplicationController
 
   def destroy
   end
+
   private
 
     def image_params
-      params.require(:image).permit(:image, :category_id, :image_title, :image_description, :image_file_size, :remote_image_url)
+      params.require(:image).permit(:image, :category_id, :image_title, :image_description, :image_file_size, :image_content_type, :remote_image_url)
     end
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,16 @@
+# frozen_string_literal: true
+# Image database class
 class Image < ApplicationRecord
-    belongs_to :category
+  belongs_to :category
+  mount_uploader :image, ImageUploader
+  before_save :update_image_attributes
+
+  private
+
+    def update_image_attributes
+      if image.present? && image_changed?
+        self.image_content_type = image.file.content_type
+        self.image_file_size = image.file.size
+      end
+    end
 end
