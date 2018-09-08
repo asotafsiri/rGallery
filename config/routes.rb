29e883eba44c7e6429dfa9570d2522e32995bee1
asotafsiri <aso.tafsiri@gmail.com>
2018-09-08 18:01:59 +0430

    improving the routepath file

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,10 @@
+# frozen_string_literal: true
+
+# routing pages
 Rails.application.routes.draw do
-  get 'images/index'
-  get 'images/show'
   get 'images/new'
-  get 'images/edit'
-  get 'images/create'
-  get 'images/update'
-  get 'images/destroy'
-  get 'categories/index'
-  get 'categories/show'
-  get 'categories/new'
-  get 'categories/edit'
-  get 'categories/create'
-  get 'categories/update'
-  get 'categories/destroy'
+  get 'upload' => 'images#new', :as => 'upload'
+  root 'images#index'
+  resources :images, :categories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
