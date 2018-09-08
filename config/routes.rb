950a461489ea8842de9e2bba363da9362de6e416
asotafsiri <aso.tafsiri@gmail.com>
2018-09-08 17:15:53 +0430

    initializing rGallery and install neccessoryy gemfiles

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'categories/index'
+  get 'categories/show'
+  get 'categories/new'
+  get 'categories/edit'
+  get 'categories/create'
+  get 'categories/update'
+  get 'categories/destroy'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
